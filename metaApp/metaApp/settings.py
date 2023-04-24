76897817e78712e8c0a32f324668dbad2262beae
Dicertonodio <59653269+Dicertonondio@users.noreply.github.com>
2023-04-24 15:35:42 +0200

    Update settings.py

--- metaApp/metaApp/settings.py
+++ metaApp/metaApp/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%o^3(9#h&l=9-pa@bwnh&d4s72x!_j^yz-)t85a@fp9$nl$o17'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
